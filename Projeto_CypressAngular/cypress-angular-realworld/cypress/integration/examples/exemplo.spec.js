2727cf4753d88a682699e0eefd52fbe2073520c1
Cristianeasreis <cristianeasreis@hotmail.com>
2020-12-22 19:06:22 -0300

    estuando Cypress angular

---
+++ Projeto_CypressAngular/cypress-angular-realworld/cypress/integration/examples/exemplo.spec.js
@@ -0,0 +1,8 @@
+const { isatty } = require("tty")
+
+describe('Primeiro Teste', () => {
+    it('Exemplo Cypress', () => {
+crypto.visit('https://example.cypress.io')
+expect(true).to.equal(true)
+    })
+})
